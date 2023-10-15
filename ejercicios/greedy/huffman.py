1ec6c9b4c47d55bb1952c82c8403a37a97e9cd42
Alexia Aroa <alexia.1509rojo@gmail.com>
2023-10-15 01:04:40 -0300

    Arbol de Huffman

---
+++ ejercicios/greedy/huffman.py
@@ -0,0 +1,12 @@
+# 1) Crear tabla de frecuencias.
+
+# 2) Armamos árboles -> Dato: letra y frecuencia.
+
+# 3) Metemos todos los árboles en un Heap de mínimos (en base a la frecuencia).
+
+# 4) Desencolo 2 elementos y los encolo con la suma de ambas frecuencias.
+#    Repito hasta tener 1 solo elemento.
+
+# 5) Una vez armado el árbol de Huffman, todos los izquierdos valen 0, y los derechos valen 1.
+
+# 6) Formar BIN: vamos desde la raíz y seguimos el camino hacia la letra que queremos.
