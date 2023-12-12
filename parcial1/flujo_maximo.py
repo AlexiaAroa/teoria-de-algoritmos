acc82a33f418b5a5c5b239085c5f42800cf3bc0d
Alexia Aroa <alexia.1509rojo@gmail.com>
2023-12-11 23:30:40 -0300

    Flujo maximo, corte minimo

---
+++ parcial1/flujo_maximo.py
@@ -0,0 +1,14 @@
+"""
+Hacer un seguimiento de obtener el flujo máximo en la red de transporte del dorso, realizando las modificaciones
+previas que fueran necesarias. Luego, definir cuáles son los dos conjuntos del corte mínimo en dicha red.
+
+Flujo maximo = 10
+Como el grafo corresponde a una red de flujo, el corte minimo tiene capacidad
+igual al flujo maximo.
+
+Conjunto 1: 
+S-V-U
+Conjunto 2:
+T-W-Z-X-T1(super sumidero)
+
+"""
